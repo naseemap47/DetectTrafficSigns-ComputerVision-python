e5960befb50ce8bff53052447f4d67f89acd643d
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-05 13:29:14 +0530

    Created train, val, test generators, using create_generates from my_utils

--- traffic_signs.py
+++ traffic_signs.py
@@ -1,9 +1,12 @@
 from my_utils import data_to_list
 from sklearn.model_selection import train_test_split
-from my_utils import plot_training_data
+from my_utils import plot_training_data, create_generators
+from deeplearing_model import trafficSign_model
+from keras.callbacks import EarlyStopping
 
 # Switches
-PLOT = True
+PLOT = False
+TRAIN = True
 
 # All image data into a single list
 image_list, class_no = data_to_list(
@@ -22,3 +25,11 @@ if PLOT:
         x_train=x_train,
         y_train=y_train
     )
+
+# Training
+if TRAIN:
+    train_generators, val_generators, test_generators = create_generators(
+        64, 43, x_train, y_train, x_val, y_val, x_test, y_test
+    )
+    
+
