94f1110d0357a81ede27fe4623e8106dcca7ae58
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-05 13:27:28 +0530

    write function to create generates (Train, Val, Test)

--- my_utils.py
+++ my_utils.py
@@ -1,8 +1,9 @@
 import os
 import cv2
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils.np_utils import to_categorical
 
 
 def data_to_list(path_to_data):
@@ -34,3 +35,39 @@ def plot_training_data(no_class, x_train, y_train):
     plt.xlabel("Class number")
     plt.ylabel("Number of images")
     plt.show()
+
+
+def create_generators(batch_size, no_class,
+                      x_train, y_train,
+                      x_val, y_val,
+                      x_test, y_test):
+    # to_categorical
+    y_train = to_categorical(y_train, no_class)
+    y_val = to_categorical(y_val, no_class)
+    y_test = to_categorical(y_test, no_class)
+
+    # Preprocessor
+    train_preprocessor = ImageDataGenerator(
+        rescale=1/255,
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1
+    )
+    val_preprocessor = ImageDataGenerator(rescale=1/255)
+    test_preprocessor = ImageDataGenerator(rescale=1/255)
+    train_generators = train_preprocessor.flow(
+        x_train, y_train,
+        batch_size=batch_size,
+        shuffle=True
+    )
+    val_generators = val_preprocessor.flow(
+        x_val, y_val,
+        batch_size=batch_size,
+        shuffle=False
+    )
+    test_generators = test_preprocessor.flow(
+        x_test, y_test,
+        batch_size=batch_size,
+        shuffle=False
+    )
+    return train_generators, val_generators, test_generators
