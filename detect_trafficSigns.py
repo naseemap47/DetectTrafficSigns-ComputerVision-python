a640943873b84c1eceba2ac901a2e3b8c4d79054
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-05 15:53:16 +0530

    loaded model and labels

--- detect_trafficSigns.py
+++ detect_trafficSigns.py
@@ -1,6 +1,19 @@
 import cv2
+from keras.models import load_model
+import pandas as pd
+
+frameWidth = 640         # CAMERA RESOLUTION
+frameHeight = 480
 
 cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+# Model
+model = load_model('/home/naseem/PycharmProjects/DetectTrafficSigns-ComputerVision-python/Model.h5')
+labels = pd.read_csv('labels.csv')
+# print(labels['Name'])
+traffic_labels = labels['Name']
 
 while True:
     success, img = cap.read()
