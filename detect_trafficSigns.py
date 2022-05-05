874a6542506964ea7cf8ea75417edcf3944f98bc
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-05 16:26:42 +0530

    predicting traffic signs properly with web-cam, printe the result correclty

--- detect_trafficSigns.py
+++ detect_trafficSigns.py
@@ -1,6 +1,8 @@
 import cv2
 from keras.models import load_model
 import pandas as pd
+from keras.preprocessing.image import img_to_array
+import numpy as np
 
 frameWidth = 640         # CAMERA RESOLUTION
 frameHeight = 480
@@ -16,9 +18,19 @@ labels = pd.read_csv('labels.csv')
 traffic_labels = labels['Name']
 
 while True:
-    success, img = cap.read()
+    success, img_og = cap.read()
+    img_rgb = cv2.cvtColor(img_og, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img_rgb, (32, 32))
+    img = img.astype('float32')/255
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
 
-    cv2.imshow('Webcam', img)
+    # Prediction
+    prediction = model.predict(img)[0]
+    predict = traffic_labels[prediction.argmax()]
+    print(predict)
+
+    cv2.imshow('Webcam', img_og)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
